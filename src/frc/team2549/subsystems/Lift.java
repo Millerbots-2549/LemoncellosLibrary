4b3029653cfefcc3295778e58d1d86406bcbd23e
2549-Elitebook <millerbots@gmail.com>
2018-12-20 15:54:17 -0600

    test1

---
+++ src/frc/team2549/subsystems/Lift.java
@@ -0,0 +1,18 @@
+package frc.team2549.subsystems;
+
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Lift extends Subsystem {
+    //Jaguar = new Jaguar.
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+
+    public void initDefaultCommand() {
+        // TODO: Set the default command, if any, for a subsystem here. Example:
+        //    setDefaultCommand(new MySpecialCommand());
+    }
+}
+
